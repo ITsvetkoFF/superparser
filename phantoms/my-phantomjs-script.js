633cfcaebe0b1ac63efbb1d8dbf6bb3173afd233
itsvetkoff <tsvetkoff@i.ua>
2015-06-03 02:03:24 +0300

    status

--- phantoms/my-phantomjs-script.js
+++ phantoms/my-phantomjs-script.js
@@ -5,7 +5,7 @@ var url = system.args[1]; // haardcoded arg with site
 page.open(url, function (status) {
     var filename = "pages/" + Math.random().toString(36).substring(7) + ".jpeg";
     //page.viewportSize = { width: 1024, height: 600 };
-
+    console.log("status: ", status);
     page.render(filename, {format: 'jpeg', quality: '20'});
     system.stdout.write(filename);
     phantom.exit();
